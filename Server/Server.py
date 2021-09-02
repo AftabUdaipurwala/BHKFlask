1e7c3fc9ed332442a6f6b83db9cd37d304060332
udaipurwalaa <udaipurwalaa@gmail.com>
2021-09-02 20:08:58 +0530

    Added Utils file

---
+++ Server/Server.py
@@ -0,0 +1,26 @@
+from flask import Flask,request,jsonify
+import Util
+app = Flask(__name__)
+
+@app.route("/Hello")
+def get_location_names():
+  response =jsonify({
+    'locations':Util.get_location_names()
+  })
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+
+@app.route("/Predict Home Price", methods=['GET','POST'])
+def predict_home_prices():
+    bath=int(request.form['bath'])
+    bhk=int(request.form['bhk'])
+    location=request.form(['location'])
+    TotalSqftAct=float(request.form['TotalSqftAct'])
+
+    response = jsonify({
+      'estimated_price':Util.get_estimated_price(location,bath,bhk,TotalSqftAct)
+    })
+
+
+if __name__ == "__main__":
+  app.run()
